e7ed6ffc5a98af692471857a7661acffa734878f
Italo <italoc.alb@gmail.com>
2022-05-10 19:58:24 -0300

    MolecularDynamics class

--- main.py
+++ main.py
@@ -1,18 +1,77 @@
-import matplotlib.pyplot as plt
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#import matplotlib.pyplot as plt
+import math
+import numpy
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MolecularDynamics(object):
+    def __init__(self, RadiusCutoff:float = 2,
+                 InteractionStrength:float = 1,
+                 LengthScale:float = 1,
+                 NumberOfParticles:int = 9):
+
+        self._N = NumberOfParticles
+        self._RC = RadiusCutoff
+        self._EPS = InteractionStrength
+        self._SIG = LengthScale
 
 
-# Press the green button in the gutter to run the script.
+
+    @property
+    def _position(self):
+        return self._pos
+
+    @_position.setter
+    def _position(self, _p):
+        self._pos = _p
+
+    @property
+    def _velocity(self):
+        return self._vel
+
+    @_velocity.setter
+    def _velocity(self, _v):
+        self._vel = _v
+
+    @property
+    def _force(self):
+        return self._for
+
+    @_force.setter
+    def _force(self, _f):
+        self._for = _f
+
+    def _lennard_jones_force(self, r:numpy.ndarray):
+        _f = 48 * self._EPS / r * (numpy.power(self._SIG / r, 12) - 1 / 2 * numpy.power(self._SIG / r, 6))
+        return _f
+
+    def _update_force(self):
+        _force = numpy.zeros((self._N,), dtype=float)
+        for i in range(self._N):
+            # difference between two particle axis-wise
+            _diff = self._position - self._position[i]
+            # delete identical particle
+            _diff = numpy.delete(_diff, i, axis=0)
+
+            # absolute distance between two particle
+            _r = numpy.sqrt(numpy.sum(numpy.power(_diff, 2), axis=-1))
+
+            # distance unitary vector of each pair
+            _rh = numpy.transpose(numpy.transpose(_diff) / _r)
+
+            _force[i] = numpy.sum(self._lennard_jones_force(_rh), axis=0)
+        return _force
+
+    def update(self, h):
+        self._position = self._position + h * self._velocity + h ** 2 * self._force / 2
+        _temp = self._update_force()
+        self._velocity = self._velocity + h * [_temp + self._force] / 2
+        self._force = _temp
+        return
+
+def main():
+    md = MolecularDynamics()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
